9d8763b2b89516b2b83554e795785d180123754f
Himash <Himash@DESKTOP-G8163TA>
2021-03-27 13:47:13 +0530

    Employee Service Modification

--- src/main/java/com/prime/hrm/controller/ShiftAllocationController.java
+++ src/main/java/com/prime/hrm/controller/ShiftAllocationController.java
@@ -105,89 +105,89 @@ public class ShiftAllocationController {
 		return list;
 	}
 
-	@Async
-	@PostMapping("/assignShift")
-	public String assignShift(@RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate,
-			@RequestParam("shiftId") String shiftId, @RequestParam("shiftName") String shiftName,
-			@RequestParam("startTime") String startTime, @RequestParam("endTime") String endTime,
-			@RequestParam("departmentId") String departmentId, @RequestParam("departmentName") String departmentName,
-			@RequestParam(value = "employeeId", required = false) String employeeId,
-			@RequestParam(value = "allEmployees", required = false) int allEmployees,
-			@RequestParam(value = "companyId", required = false) String companyId) {
-
-		String empId;
-		String employeeName;
-		List<ShiftAllocation> list = new ArrayList<>();
-
-		Date d1;
-		Date d2;
-
-		d1 = Date.valueOf(startDate);
-		d2 = Date.valueOf(endDate);
-
-		try {
-
-			if (allEmployees == 1) {
-
-				//List<EmployeeDetails> department = employeeService.filterRelatedData(departmentId);
-				List<EmployeeDetails> department = employeeService.filterEmployeesByDepartmentAndCompany(departmentId,
-						companyId);
-
-				for (int i = 0; i < department.size(); i++) {
-					empId = department.get(i).getDetailsPK().getEmpID().getEmpID().toString();
-					employeeName = department.get(i).getDetailsPK().getEmpID().getName().toString() + " "
-							+ department.get(i).getDetailsPK().getEmpID().getLastname();
-					System.out.println(empId);
-					for (LocalDate date = d1.toLocalDate(); date
-							.isBefore(d2.toLocalDate().plusDays(1)); date = date.plusDays(1)) {
-						ShiftAllocationPK shiftAllocationPK = new ShiftAllocationPK();
-						ShiftAllocation allocation = new ShiftAllocation();
-						Date shiftDate = Date.valueOf(date);
-						shiftAllocationPK.setDate(shiftDate);
-						shiftAllocationPK.setShiftId(shiftId);
-						shiftAllocationPK.setEmployeeId(empId);
-						allocation.setShiftAllocationPK(shiftAllocationPK);
-						allocation.setShiftName(shiftName);
-						allocation.setStartTime(startTime);
-						allocation.setEndTime(endTime);
-						allocation.setEmployeeName(employeeName);
-						allocation.setDepartmentId(departmentId);
-						allocation.setDepartmentName(departmentName);
-						allocation.setCompanyId(companyId);
-						list.add(allocation);
-					}
-				}
-				shiftAllocationService.saveShiftAllocations(list);
-			} else if (allEmployees == 0) {
-
-				Employee employee = employeeService.getEmployeeByCompany(employeeId, companyId);
-				employeeName = employee.getName() + " " + employee.getLastname();
-
-				for (LocalDate date = d1.toLocalDate(); date
-						.isBefore(d2.toLocalDate().plusDays(1)); date = date.plusDays(1)) {
-					ShiftAllocationPK shiftAllocationPK = new ShiftAllocationPK();
-					ShiftAllocation allocation = new ShiftAllocation();
-					Date shiftDate = Date.valueOf(date);
-					shiftAllocationPK.setDate(shiftDate);
-					shiftAllocationPK.setShiftId(shiftId);
-					shiftAllocationPK.setEmployeeId(employeeId);
-					allocation.setShiftAllocationPK(shiftAllocationPK);
-					allocation.setShiftName(shiftName);
-					allocation.setStartTime(startTime);
-					allocation.setEndTime(endTime);
-					allocation.setEmployeeName(employeeName);
-					allocation.setDepartmentId(departmentId);
-					allocation.setDepartmentName(departmentName);
-					allocation.setCompanyId(companyId);
-					list.add(allocation);
-				}
-				shiftAllocationService.saveShiftAllocations(list);
-			}
-			return "redirect:/ShiftAllocation";
-
-		} catch (Exception e) {
-			System.out.println("Error");
-		}
-		return "ShiftAllocation";
-	}
+//	@Async
+//	@PostMapping("/assignShift")
+//	public String assignShift(@RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate,
+//			@RequestParam("shiftId") String shiftId, @RequestParam("shiftName") String shiftName,
+//			@RequestParam("startTime") String startTime, @RequestParam("endTime") String endTime,
+//			@RequestParam("departmentId") String departmentId, @RequestParam("departmentName") String departmentName,
+//			@RequestParam(value = "employeeId", required = false) String employeeId,
+//			@RequestParam(value = "allEmployees", required = false) int allEmployees,
+//			@RequestParam(value = "companyId", required = false) String companyId) {
+//
+//		String empId;
+//		String employeeName;
+//		List<ShiftAllocation> list = new ArrayList<>();
+//
+//		Date d1;
+//		Date d2;
+//
+//		d1 = Date.valueOf(startDate);
+//		d2 = Date.valueOf(endDate);
+//
+//		try {
+//
+//			if (allEmployees == 1) {
+//
+//				//List<EmployeeDetails> department = employeeService.filterRelatedData(departmentId);
+//				List<EmployeeDetails> department = employeeService.filterEmployeesByDepartmentAndCompany(departmentId,
+//						companyId);
+//
+//				for (int i = 0; i < department.size(); i++) {
+//					empId = department.get(i).getDetailsPK().getEmpID().getEmpID().toString();
+//					employeeName = department.get(i).getDetailsPK().getEmpID().getName().toString() + " "
+//							+ department.get(i).getDetailsPK().getEmpID().getLastname();
+//					System.out.println(empId);
+//					for (LocalDate date = d1.toLocalDate(); date
+//							.isBefore(d2.toLocalDate().plusDays(1)); date = date.plusDays(1)) {
+//						ShiftAllocationPK shiftAllocationPK = new ShiftAllocationPK();
+//						ShiftAllocation allocation = new ShiftAllocation();
+//						Date shiftDate = Date.valueOf(date);
+//						shiftAllocationPK.setDate(shiftDate);
+//						shiftAllocationPK.setShiftId(shiftId);
+//						shiftAllocationPK.setEmployeeId(empId);
+//						allocation.setShiftAllocationPK(shiftAllocationPK);
+//						allocation.setShiftName(shiftName);
+//						allocation.setStartTime(startTime);
+//						allocation.setEndTime(endTime);
+//						allocation.setEmployeeName(employeeName);
+//						allocation.setDepartmentId(departmentId);
+//						allocation.setDepartmentName(departmentName);
+//						allocation.setCompanyId(companyId);
+//						list.add(allocation);
+//					}
+//				}
+//				shiftAllocationService.saveShiftAllocations(list);
+//			} else if (allEmployees == 0) {
+//
+//				Employee employee = employeeService.getEmployeeByCompany(employeeId, companyId);
+//				employeeName = employee.getName() + " " + employee.getLastname();
+//
+//				for (LocalDate date = d1.toLocalDate(); date
+//						.isBefore(d2.toLocalDate().plusDays(1)); date = date.plusDays(1)) {
+//					ShiftAllocationPK shiftAllocationPK = new ShiftAllocationPK();
+//					ShiftAllocation allocation = new ShiftAllocation();
+//					Date shiftDate = Date.valueOf(date);
+//					shiftAllocationPK.setDate(shiftDate);
+//					shiftAllocationPK.setShiftId(shiftId);
+//					shiftAllocationPK.setEmployeeId(employeeId);
+//					allocation.setShiftAllocationPK(shiftAllocationPK);
+//					allocation.setShiftName(shiftName);
+//					allocation.setStartTime(startTime);
+//					allocation.setEndTime(endTime);
+//					allocation.setEmployeeName(employeeName);
+//					allocation.setDepartmentId(departmentId);
+//					allocation.setDepartmentName(departmentName);
+//					allocation.setCompanyId(companyId);
+//					list.add(allocation);
+//				}
+//				shiftAllocationService.saveShiftAllocations(list);
+//			}
+//			return "redirect:/ShiftAllocation";
+//
+//		} catch (Exception e) {
+//			System.out.println("Error");
+//		}
+//		return "ShiftAllocation";
+//	}
 }
--- src/main/java/com/prime/hrm/repository/DesignationMasterRepository.java
+++ src/main/java/com/prime/hrm/repository/DesignationMasterRepository.java
@@ -1,14 +1,18 @@
 package com.prime.hrm.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.prime.hrm.entity.DesignationMaster;
 
 public interface DesignationMasterRepository extends CrudRepository<DesignationMaster, String> {
 
-	
-
 	@Query(value = "SELECT (max(d.did)+1) FROM DesignationMaster d ")
 	public String maxDesignationID();
+	
+	@Query(value="SELECT dm FROM DesignationMaster dm WHERE dm.company.comID = :companyId")
+	public List<DesignationMaster> getAllDesignationsByCompany(@Param("companyId") String companyId);
 }
